631c48c91fceee1c83997bf9dc97ff14971f8840
Jordy Camacho <jordycamacho225@gmail.com>
2025-02-15 15:57:29 +0100

    remove commented-out code for user permissions and roles initialization

--- src/main/java/com/example/bordados/BordadosApplication.java
+++ src/main/java/com/example/bordados/BordadosApplication.java
@@ -11,40 +11,4 @@ public class BordadosApplication {
         public static void main(String[] args) {
                 SpringApplication.run(BordadosApplication.class, args);
         }
-
-        /*@Bean
-        CommandLineRunner init(UserRepository userRepository) {
-                return args -> {
-                        // Create PERMISSIONS
-                        Permission createPermission = Permission.builder()
-                                        .name("CREATE")
-                                        .build();
-
-                        Permission readPermission = Permission.builder()
-                                        .name("READ")
-                                        .build();
-
-                        Permission updatePermission = Permission.builder()
-                                        .name("UPDATE")
-                                        .build();
-
-                        Permission deletePermission = Permission.builder()
-                                        .name("DELETE")
-                                        .build();
-
-                        // Create ROLES
-                        Role roleAdmin = Role.builder()
-                                        .roleEnum(RoleEnum.ADMIN)
-                                        .Permission(Set.of(createPermission, readPermission, updatePermission,
-                                                        deletePermission))
-                                        .build();
-
-                        Role roleUser = Role.builder()
-                                        .roleEnum(RoleEnum.USER)
-                                        .Permission(Set.of(readPermission))
-                                        .build();
-
-                        
-                };
-        }*/
 }
