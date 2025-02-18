b21e280c4fa09436be6bc0a50140de2958b636fa
Jordy Camacho <jordycamacho225@gmail.com>
2025-02-18 16:58:18 +0100

    add methods to UserController for retrieving products by category and fetching categories with subcategories

--- src/main/java/com/example/bordados/controller/UserController.java
+++ src/main/java/com/example/bordados/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.bordados.controller;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +14,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.bordados.DTOs.CategorySubCategoryDTO;
 import com.example.bordados.DTOs.UserDTO;
+import com.example.bordados.model.Product;
 import com.example.bordados.service.IUserService;
+import com.example.bordados.service.ServiceImpl.CategoryServiceImpl;
+import com.example.bordados.service.ServiceImpl.ProductServiceImpl;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,11 +37,28 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private CategoryServiceImpl categoryService;
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @ModelAttribute("categoriesWithSub")
+    public List<CategorySubCategoryDTO> getCategoriesWithSubCategories() {
+        return categoryService.getAllCategoriesWithSubCategories();
+    }
+    
     @GetMapping("")
     public String getProducts() {
         return "user/index";
     }
 
+    @GetMapping("/categoria/{id}")
+    public String getProductsByCategoryOrSubCategory(@PathVariable Long id, Model model) {
+        List<Product> products = productService.getProductBySubCategory(id);
+        model.addAttribute("products", products);
+        return "user/products";
+    }
     @GetMapping("/login")
     public String getLogin(@RequestParam(value = "error", required = false) String error,
             @RequestParam(value = "logout", required = false) String logout,
