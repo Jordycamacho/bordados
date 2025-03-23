14a6536bf548cdb1bd4392cfc9975c7f2827bda4
Jordy Camacho <jordycamacho225@gmail.com>
2025-03-23 22:49:37 +0100

    enhance resource handling: improve image resource configuration and update upload directory path

--- src/main/java/com/example/bordados/config/ResourceWebConfiguration.java
+++ src/main/java/com/example/bordados/config/ResourceWebConfiguration.java
@@ -3,11 +3,18 @@ package com.example.bordados.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class ResourceWebConfiguration implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/", "classpath:/images/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 }
